bb52aab341277b6bfbf2cb6bb943ad94572c6103
kevinbezos <wezuartsbackup@outlook.com>
2026-01-12 16:01:45 +0100

    prettier + yml

---
+++ src/App.tsx
@@ -0,0 +1,4 @@
+import { createRoot } from "react-dom/client";
+import React from "react";
+import { Application } from "./components/Application.js";
+createRoot(document.getElementById("root")!).render(<Application />);

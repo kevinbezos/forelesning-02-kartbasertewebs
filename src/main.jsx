4844568295a341a9316db5ec01cf3aaa051194ef
kevinbezos <wezuartsbackup@outlook.com>
2026-01-12 14:58:49 +0100

    added all prettier .

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,3 @@
-import {createRoot} from "react-dom/client";
-import React from"react"
-createRoot(document.getElementById("root")).render(<h1>Hello React</h1>)
+import { createRoot } from "react-dom/client";
+import React from "react";
+createRoot(document.getElementById("root")).render(<h1>Hello React</h1>);
